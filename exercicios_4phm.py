d2e444a6475da8edb73a9365ef463d72d37d4611
Murirdosee <107406748+Murirdosee@users.noreply.github.com>
2022-06-27 12:20:20 -0300

    Add files via upload

---
+++ exercicios_4phm.py
@@ -0,0 +1,18 @@
+public class exercicio_5 {
+    
+public static void main(String[] args) {
+    for (int i = 2; i <= 123; i++){
+        if (primo(i))
+        System.out.println(i);
+    }
+}  
+
+public static boolean primo(int x){
+    
+    for (int z = 2; z < x; z++) {
+        if (x % z == 0)
+        return false;
+    }
+    return true;
+ }
+}
