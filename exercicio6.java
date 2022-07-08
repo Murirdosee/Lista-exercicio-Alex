26f7338002c847ff140d0464063898a358ee4c63
Murirdosee <107406748+Murirdosee@users.noreply.github.com>
2022-07-08 08:27:27 -0300

    Add files via upload

---
+++ exercicio6.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class exer6 {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            // Atributos
+            int horas;
+            float salario;
+
+            // Input
+            System.out.print("Quantidade de horas de trabalho: ");
+            horas = input.nextInt();
+
+            // Cálculo
+            salario = (float) (horas * 10.25);
+
+            // Output
+            System.out.println("Salário pago R$" + salario);
+        }
+    }
+}
