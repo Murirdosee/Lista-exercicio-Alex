42975cd7cde1d80ba45b3edfb0c4f1dd59672a70
Murirdosee <107406748+Murirdosee@users.noreply.github.com>
2022-07-11 11:55:38 -0300

    Update exercicio9.java

--- exercicio9.java
+++ exercicio9.java
@@ -1,17 +1,36 @@
+import javax.swing.JOptionPane;
 import java.util.Scanner;
 
 public class exer9 {
     public static void main(String[] args) {
-        try (Scanner input = new Scanner(System.in)){
-            // Atributos
-            int idade;
-            
-            // Input
-            System.out.println("Informe sua idade: ");
-            idade = input.nextInt();
-
-            // Condições
-
+        Scanner entrada = new Scanner(System.in);
+        Integer idade;
+        System.out.println("Digite a idade: ");
+        idade = entrada.nextInt();
+        if (idade <= 3) {
+            JOptionPane.showMessageDialog(null, " bebe");
         }
-    }   
+        if ((idade >= 4) && (idade <= 9)) {
+            JOptionPane.showMessageDialog(null, " criançinha");
+        }
+        if ((idade >= 10) && (idade <= 13)) {
+            JOptionPane.showMessageDialog(null, "pré-adolescente");
+        }
+        if ((idade >= 14) && (idade <= 17)) {
+            JOptionPane.showMessageDialog(null, "adolescente");
+        }
+        if ((idade >= 18) && (idade <= 25)) {
+            JOptionPane.showMessageDialog(null, "jovem adulto");
+        }
+        if ((idade >= 26) && (idade <= 50)) {
+            JOptionPane.showMessageDialog(null, " adulto");
+        }
+        if ((idade >= 51) && (idade <= 65)) {
+            JOptionPane.showMessageDialog(null, "ja pode chamar de senhor ");
+        }
+        if (idade >= 66) {
+            JOptionPane.showMessageDialog(null, "Veio pra caramba");
+        }
+
+    }
 }
